f787f706ebbb419ae988c1f742bfb0d50074db4c
Aluno <joao12308@hotmail.com>
2019-04-30 20:06:27 -0300

    exercicio que não sei fazer

--- file.cpp
+++ file.cpp
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+FILE* abrir_arquivo(char *fname)
 
 int main(){
 
   FILE *fp;
   return 0;
 
-  fp = fopen("data.dat", "");
+  if(abrir_arquivo = NULL){
+
+    fp = fopen("data.dat", "wb");
+  }else{
+    
+  }
+
+
+
 
 }
