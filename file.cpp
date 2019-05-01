3c0c3a266f2f5c30eaff1022ded383dfc75b27be
Aluno <joao12308@hotmail.com>
2019-04-30 21:28:46 -0300

    exercicio da aula 05 pronto file.cpp

--- file.cpp
+++ file.cpp
@@ -2,30 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-FILE* abrir_arquivo(char *fname)
+FILE* abrir_arquivo (char *fname)
+{
+	FILE *fp;
 
-int main(){
+	fp = fopen(fname, "rb+");
 
-  FILE *fp;
-  return 0;
+	if (fp == NULL) {
+		fp = fopen(fname, "wb");
 
-  fp = fopen("data.dat", "rb+");
+		if (fp == NULL) {
+			printf("impossivel criar arquivo %s\n", fname);
+			exit(1);
+		}
 
-  if(fp == NULL){
-    fp = fopen(fname, "wb");
+		fclose(fp);
 
-    if(fp == NULL){
-      printf("impossivel abrir arquivo %s\n", fname);
-      exit(1);
-    }
-    fclose(fp);
+		fp = fopen(fname, "rb+");
 
-    fp = fopen(fname, "rb+");
+		if (fp == NULL) {
+			printf("impossivel abrir arquivo %s\n", fname);
+			exit(1);
+		}
+	}
 
-    if(fp == NULL){
-      printf("impossivel criar arquivo%s\n", fname);
-      exit(1);
-    }
-  }
+	return fp;
+}
 
+void desastre ()
+{
+	int n;
+	char d[300];
+
+	for (n=0; n<300; n++)
+		d[n] = 1;
+}
+
+void ler_escrever ()
+{
+	char buffer_leitura[100];
+	FILE *fp;
+	char str[50] = "eduardo";
+	int n;
+
+	fp = abrir_arquivo("data.dat");
+
+	n = fread(buffer_leitura, sizeof(char), 100, fp);
+	buffer_leitura[n] = 0;
+
+	printf("%s\n", buffer_leitura);
+
+	fclose(fp);
+	fp = abrir_arquivo("data.dat");
+
+	fwrite(str, sizeof(char), strlen(str), fp);
+
+	fclose(fp);
+}
+
+int main ()
+{
+	desastre();
+	ler_escrever();
+
+	return 0;
 }
