0661c3f8f77dd9f1316fd4a36b8e583470b5453c
Aluno <joao12308@hotmail.com>
2019-04-30 21:24:50 -0300

    exercicio teste 02

--- file.cpp
+++ file.cpp
@@ -9,14 +9,23 @@ int main(){
   FILE *fp;
   return 0;
 
-  if(abrir_arquivo = NULL){
+  fp = fopen("data.dat", "rb+");
 
-    fp = fopen("data.dat", "wb");
-  }else{
-    
+  if(fp == NULL){
+    fp = fopen(fname, "wb");
+
+    if(fp == NULL){
+      printf("impossivel abrir arquivo %s\n", fname);
+      exit(1);
+    }
+    fclose(fp);
+
+    fp = fopen(fname, "rb+");
+
+    if(fp == NULL){
+      printf("impossivel criar arquivo%s\n", fname);
+      exit(1);
+    }
   }
-
-
-
 
 }
